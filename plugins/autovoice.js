ce2e5f679abd01526580171b61411c6eaa889ae7
Nethmina Official <nethminaofc.dev@gmail.com>
2026-04-28 16:49:07 +0530

    Update autovoice.js

--- plugins/autovoice.js
+++ plugins/autovoice.js
@@ -5,7 +5,7 @@ cmd({
 },    
 async (conn, mek, m, { from, body, isOwner, readEnv }) => {
     try {
-        // බොට් එවපු මැසේජ් එකක් නම් රිප්ලයි කරන්න එපා
+        
         if (m.key.fromMe) return;
 
         const msgText = body ? body.toLowerCase().trim() : "";
@@ -15,22 +15,22 @@ async (conn, mek, m, { from, body, isOwner, readEnv }) => {
             
             let voiceUrl = '';
 
-            // වචනය පරීක්ෂා කිරීම
+            
             if (msgText === 'hi' || msgText === 'hello') {
                 voiceUrl = 'https://github.com/Nethmina-dev/BOT-DATA/raw/refs/heads/main/Voice-notes/hi%23old.mp3';
             } else if (msgText === 'mk' || msgText === 'මොකෝ') {
                 voiceUrl = 'https://github.com/Nethmina-dev/BOT-DATA/raw/refs/heads/main/Voice-notes/Mk.mp3';
             }
 
-            // අදාළ වචනය හමු වුණොත් පමණක් Voice Note එක යැවීම
+            
             if (voiceUrl !== '') {
-                // Recording... status එක පෙන්වන්න
+                
                 await conn.sendPresenceUpdate('recording', from);
 
                 await conn.sendMessage(from, { 
                     audio: { url: voiceUrl }, 
                     mimetype: 'audio/mpeg', 
-                    ptt: true // මේක true නිසා Voice Note එකක් ලෙස රවුමට පෙනේ
+                    ptt: false
                 }, { quoted: mek });
             }
        }
