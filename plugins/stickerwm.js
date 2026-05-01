094699b76f80927ccc70c30e33251a7a64c24929
Official-Nethmina <nbhashitha159@gmail.com>
2026-05-01 17:58:09 +0530

    Update stickerwm.js

--- plugins/stickerwm.js
+++ plugins/stickerwm.js
@@ -17,7 +17,7 @@ async (conn, mek, m, { from, reply, quoted, body }) => {
 
         if (!isSticker) return reply("*You did not reply to a sticker. Please reply to a sticker. 🙂*");
 
-        let pack = "💟 ＮＥＴＨＭＩＮＡ - ＳＴＩＣＫＥＲＳ 💟"; 
+        let pack = "💟 𝙽𝙴𝚃𝙷𝙼𝙸𝙽𝙰 - 𝚂𝚃𝙸𝙲𝙺𝙴𝚁𝚂 💟"; 
         let author = "© 🧑🏻‍💻 ɴᴇᴛʜᴍɪɴᴀ ᴏꜰꜰɪᴄɪᴀʟ ᴄᴏᴍᴍᴜɴɪᴛʏ 🧑🏻‍💻";
 
         if (body && body.includes('|')) {
