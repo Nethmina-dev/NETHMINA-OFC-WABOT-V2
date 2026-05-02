bb86545f7637a02c983601f1482ac174c083b966
isurahupa-tech <isurahupa@gmail.com>
2026-05-02 17:56:26 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,6 +1,7 @@
 const config = require('../config');
 const { cmd } = require('../command');
 const { runtime } = require('../lib/functions');
+const { exec } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
